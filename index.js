c5ba6f53a43d173ad742f366faa704de56d80a51
José Antonio <jtony.net@gmail.com>
2016-04-14 23:46:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const PORT 	= process.env.PORT || 3000;
+const app 	= express();
+
+app.get("/", (req, res)=>res.json({status: "NTask API"}));
+
+app.listen(PORT, ()=>console.log("NTask API - porta: ${PORT}"));
+
